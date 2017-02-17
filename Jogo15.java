c3fe418227913e038f1ff24667b00ae55f9045c9
André Cirne <dumahk21@gmail.com>
2017-02-17 15:50:51 +0000

    Criaçao das classes de cada metodo de procura

--- Jogo15.java
+++ Jogo15.java
@@ -2,23 +2,89 @@ import Tabuleiro;
 import java.util.Scanner;
 import LinkedList;
 import Node;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+// ---------- Metodos de enque
+class Search{
+  public HashMap map = null;
+  public int max = -1; //maximod e profundidade se colocar -1 nao existe maximo
+  public void add(Tabuleiro tabu, int altura,Ponto zero){}
+}
+
+class Gulosa extends Search{
+
+}
+
+class Astar extends Search{
+  public PriorityQueue queue;
+
+  public Astar(){
+    queue = PriorityQueue();
+  }
+  @Override
+  public void add(Tabuleiro tabu, int altura,Ponto zero){
+    queue.add(new Node(tabu,altura,true));
+  }
+}
+
+class IDFS{
+  public LinkedList queue;
+
+  public IDFS(){
+
+  }
+}
+
+class Dfs extends Search{
+    public LinkedList queue;
+
+    public Dfs(int n){
+      queue = new LinkedList();
+      max = n;
+    }
+
+    @Override
+    public void add(Tabuleiro tabu, int altura,Ponto zero){
+      queue.addFirst(new Node(tabu,altura,false)); //nao imporaa ordem desde que seja adicionado no inicio
+    }
+}
+
+class Bfs extends Search{
+  public LinkedList queue;
+
+  public Bfs(){
+    map = new HashMap();
+    queue = new LinkedList();
+  }
+  @Override
+  public void add(Tabuleiro tabu, int altura,Ponto zero){
+    if(!map.containsKey(tabu)){
+      queue.add(new Node(tabu,altura,false)); //nos tem d eser acrescentados no final da fila
+      map.put(tabu,zero)
+    }
+  }
+
+}
+//-------------
 
 class Jogo15{
   private final int lado = 4;
 
-  private void generalSearchAlgorithm(Callable<t> func){
+  private void generalSearchAlgorithm(Search procura){
     //contadores
     long numeroNos = 0; //Numero de nos gerados
     //final = new Node()
-    LinkedList queue = new LinkedList();
     while (!queue.isEmpty()) {
       Node node = queue.pop()
       if (node.tabu.equals(final)) {
-
+        System.out.println("Numero minimo de jogadas encontradas:"+" "+node.altura);
       }
       else{
-        for(Tabuleiro tabu: node.tabu.makeDescendents()){
-
+        if(procura.max != node.altura){
+          for(Tabuleiro tabu: node.tabu.makeDescendents(procura.map)){
+            procura.add(queue,tabu,node.altura+1,node.tabu.zero);
+          }
         }
       }
 
