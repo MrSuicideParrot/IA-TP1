900c03bc1f6adca0f05a4d725aa93cde9d34d628
André Cirne <dumahk21@gmail.com>
2017-02-19 21:38:03 +0000

    A

---
+++ python/jogo15.py
@@ -0,0 +1,15 @@
+import tabuleiro.Tabuleiro
+import node.Node
+
+lado = 4
+
+def generalSearchAlgorithm():
+    numeroNos = 0
+    while not fila == []:
+        node =
+
+
+def main():
+    inicial = [[int(i) for i in input().split()] for _ in range(4)]
+    final = [[int(i) for i in input().split()] for _ in range(4)]
+
---
+++ python/serach.py
@@ -0,0 +1,29 @@
+class Search:
+
+    def __init__(self,code):
+        self.code = code
+        if code is 1: # dfs
+            fila = []
+            return
+
+        if code is 2: # bfs
+            fila = []
+            dict = {}
+            return
+
+        if code is 3: # idfs
+            fila = []
+            return
+
+        if code is 4: # gulosa
+            fila = []
+            return
+
+        if code is 5: # istar
+            fila = []
+            return
+
+
+
+    def addFila(self):
+        i
